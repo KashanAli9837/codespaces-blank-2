3f10e2db7f68bb38893078d08916888f7959101c
Kashan Ali <137059086+KashanAli9837@users.noreply.github.com>
2024-10-05 14:42:41 +0000

    try1

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import SessionWrapper from "./components/SessionWrapper";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -20,11 +21,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <SessionWrapper>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </SessionWrapper>
     </html>
   );
 }
